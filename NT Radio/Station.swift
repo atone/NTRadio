2ebf9d1826f02d697f811d53bb3e67b7611a7d8a
Naitong Yu <yunaitong@gmail.com>
2018-06-29 01:01:15 +0800

    move station resources to config file

--- NT Radio/Station.swift
+++ NT Radio/Station.swift
@@ -12,36 +12,21 @@ struct Station {
     let name: String
     let url: URL
     
-    static let all: [Station]  = [
-        Station(name: "中国之声", url: URL(string: "http://ngcdn001.cnr.cn/live/zgzs/index.m3u8")!),
-        Station(name: "经济之声", url: URL(string: "http://ngcdn002.cnr.cn/live/jjzs/index.m3u8")!),
-        Station(name: "音乐之声", url: URL(string: "http://ngcdn003.cnr.cn/live/yyzs/index.m3u8")!),
-        Station(name: "经典音乐广播", url: URL(string: "http://ngcdn004.cnr.cn/live/dszs/index.m3u8")!),
-        Station(name: "中华之声", url: URL(string: "http://ngcdn005.cnr.cn/live/zhzs/index.m3u8")!),
-        Station(name: "神州之声", url: URL(string: "http://ngcdn006.cnr.cn/live/szzs/index.m3u8")!),
-        Station(name: "华夏之声", url: URL(string: "http://ngcdn007.cnr.cn/live/hxzs/index.m3u8")!),
-        Station(name: "香港之声", url: URL(string: "http://ngcdn008.cnr.cn/live/xgzs/index.m3u8")!),
-        Station(name: "民族之声", url: URL(string: "http://ngcdn009.cnr.cn/live/mzzs/index.m3u8")!),
-        Station(name: "文艺之声", url: URL(string: "http://ngcdn010.cnr.cn/live/wyzs/index.m3u8")!),
-        Station(name: "老年之声", url: URL(string: "http://ngcdn011.cnr.cn/live/lnzs/index.m3u8")!),
-        Station(name: "藏语广播", url: URL(string: "http://ngcdn012.cnr.cn/live/zygb/index.m3u8")!),
-        Station(name: "维语广播", url: URL(string: "http://ngcdn013.cnr.cn/live/wygb/index.m3u8")!),
-        Station(name: "娱乐广播", url: URL(string: "http://ngcdn014.cnr.cn/live/ylgb/index.m3u8")!),
-        Station(name: "中国交通广播", url: URL(string: "http://ngcdn016.cnr.cn/live/gsgljtgb/index.m3u8")!),
-        Station(name: "中国乡村之声", url: URL(string: "http://ngcdn017.cnr.cn/live/xczs/index.m3u8")!),
-        Station(name: "哈语广播", url: URL(string: "http://ngcdn025.cnr.cn/live/hygb/index.m3u8")!),
-        Station(name: "相声小品频道", url: URL(string: "http://ngcdn023.cnr.cn/live/xsxp/index.m3u8")!),
-        Station(name: "古典音乐频道", url: URL(string: "http://ngcdn022.cnr.cn/live/gdyy/index.m3u8")!),
-        Station(name: "长篇联播频道", url: URL(string: "http://ngcdn024.cnr.cn/live/cplb/index.m3u8")!),
-        Station(name: "中国民乐频道", url: URL(string: "http://ngcdn021.cnr.cn/live/zgmy/index.m3u8")!),
-        Station(name: "环球资讯", url: URL(string: "http://sk.cri.cn/905.m3u8")!),
-        Station(name: "Easy FM", url: URL(string: "http://sk.cri.cn/915.m3u8")!),
-        Station(name: "Hit FM", url: URL(string: "http://sk.cri.cn/887.m3u8")!),
-        Station(name: "Piano Solo Live", url: URL(string: "http://pianosolo.streamguys.net/live")!),
-        Station(name: "WTWW Transmitter 1", url: URL(string: "http://64.235.54.14:8015/")!),
-        Station(name: "WTWW Transmitter 2", url: URL(string: "http://64.235.54.14:8020/")!),
-        Station(name: "WTWW Transmitter 3", url: URL(string: "http://64.235.54.14:8026/")!)
-    ]
+    static let all = allStations()
+    
+    private static func allStations() -> [Station] {
+        var stations = [Station]()
+        let configPath = Bundle.main.path(forResource: "Stations", ofType: "plist")
+        let configDict = NSDictionary(contentsOfFile: configPath!)
+        if let stationDict = configDict as? [String: Any],
+            let stationNames = stationDict["StationName"] as? [String],
+            let stationURLs = stationDict["StationURL"] as? [String] {
+            for (index, name) in stationNames.enumerated() {
+                stations.append(Station(name: name, url: URL(string: stationURLs[index])!))
+            }
+        }
+        return stations
+    }
 }
 
 extension Station: CustomStringConvertible {
